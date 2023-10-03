306699040a5f180f6c5ea2d0b5540c183c145e2d
mj703 <akira703@protonmail.com>
2023-10-03 17:31:31 +0530

    commited

--- src/exception.py
+++ src/exception.py
@@ -11,6 +11,8 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
+#adding proper custom exception
+
 class CustomException(Exception):
     
     def __init__(self, error_message, error_detail:sys):
